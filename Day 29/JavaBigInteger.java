6c4750dc02b89b9762de69757a2246013efca97d
AkarshShaw <78737092+AkarshShaw@users.noreply.github.com>
2022-11-23 21:46:10 +0530

    Create JavaBigInteger.java

---
+++ Day 29/JavaBigInteger.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+import java.math.*;
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        BigInteger A = sc.nextBigInteger();
+        BigInteger B = sc.nextBigInteger();
+        System.out.println(A.add(B));
+        System.out.println(A.multiply(B));
+    }
+}
