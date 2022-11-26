8db94f3b1227f4161fc9bc986107b906e0bc1b57
AkarshShaw <78737092+AkarshShaw@users.noreply.github.com>
2022-11-26 16:15:20 +0530

    Create JavaSubarray.java

---
+++ Day 34/JavaSubarray.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Solution
+{
+    public static void main(String[] args)
+    {    
+        Scanner s = new Scanner(System.in);   
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        s.close();
+        int sum=0;
+        int counter=0;
+        for(int i=0;i<arr.length;i++)
+        {    
+            for(int j=i;j<arr.length;j++)
+            {       
+                sum=0;    
+                for(int k=i;k<=j;k++)
+                {
+                    sum+=arr[k];
+                }
+                if(sum<0)
+                {
+                    counter++;
+                }
+            }
+        }
+        System.out.print(counter);
+    }
+}
